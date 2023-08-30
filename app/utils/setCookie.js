5e589c327caf70d45a296587de4e6a74790ed9f5
ᴀʙᴅᴜʟʟᴀʜ ᴀʟ ɴᴏᴍᴀɴ ᴘʀɪɴᴄᴇ <prince1000bd@gmail.com>
2023-08-30 23:28:47 +0600

    first commit

--- app/utils/setCookie.js
+++ app/utils/setCookie.js
@@ -1,7 +1,5 @@
 export async function setCookie(){
-    const res = await fetch('/api/setCookie', {
-        method: 'POST'
-    })
+    const res = await fetch('/api/setCookie')
     if(res.status === 200){
         return  res.json()
     }else{
